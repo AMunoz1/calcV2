438fbcfc350379ea946b689edfabf05fbf6b4d75
Rolando  Lara <rl51054@srcs.loc>
2017-02-07 13:30:10 -0800

    Rolando Lara fixed the number limit test.

--- calculator_tests.js
+++ calculator_tests.js
@@ -28,9 +28,45 @@ QUnit.test("Change to Percentage test", function(assert) {
     allClear();
 });
     //US3: As a user, I want to be able to calculate the inverse of a number simply by pressing a button.
+QUnit.test("Calculate the inverse test", function(assert) {
+    addDigit('2');
+    inverse();
+    assert.equal(document.getElementById("screen").value, "0.5", "Passed - Expected 0.5");
+    allClear();
+});
     //US4: As a user I want to be able to calculate the factorial of a number by simply pressing a button.
     //US5: As a user I want to be able to calculate the square root of a number by simply pressing a button.
+QUnit.test("Square Root test", function(assert) {
+    addDigit('4');
+    squareRoot();
+    assert.equal(document.getElementById("screen").value, "2", "Passed - Expected 2");
+    allClear();
+});
     //US6: As a user I want to be able to calculate the square of a number by simply pressing a button.
     //US7: As a user who sometimes makes mistakes when pressing buttons on the keypad, I want to be able to press a button that clears my current input, but not the stored procedure.
+QUnit.test("Square Root test", function(assert) {
+    addDigit('4');
+    squareRoot();
+    assert.equal(document.getElementById("screen").value, "2", "Passed - Expected 2");
+    allClear();
+});
     //US8: Bug Alert! There is a bug in the calculator app! As a careless user I want to be told that I just tried to divide by zero, which I should be told is not allowed.
     //US9: Bug Alert! As an easily confused user I don't want to be able to type numbers into the screen that causes some of the numbers to disappear off the screen, thus confusing me about what I actually typed.
+QUnit.test("Number Limit test", function(assert) {
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    addDigit('1');
+    assert.equal(document.getElementById("screen").value, "1111111111111", "Passed - Expected 1111111111111");
+    allClear();
+});
